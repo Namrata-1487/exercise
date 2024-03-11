dc71bedcd55465bebb4b591b16db6530d00e3553
Namrata-1487 <namrata.shirsaat@gmail.com>
2024-03-11 14:09:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# exercise
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Namrata-1487/exercise)
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    
+  `,
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App);
